412d2898f0de978fd8fb308e43a4349f53505681
BlackCatDevel0per <333blackcat333@mail.ru>
2022-07-03 15:52:32 +0500

    Script Update

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo 'Copying files..'
 # First install the configuration files:
 sudo cp anbox.conf /etc/modules-load.d/
 sudo cp 99-anbox.rules /lib/udev/rules.d/
@@ -9,11 +10,15 @@ sudo cp -rT ashmem /usr/src/anbox-ashmem-1
 sudo cp -rT binder /usr/src/anbox-binder-1
 
 # Finally use dkms to build and install:
+echo 'Installing anbox-ashmem..'
 sudo dkms install anbox-ashmem/1
+echo 'Installing anbox-binder..'
 sudo dkms install anbox-binder/1
 
+echo '
 # Verify by loading these modules and checking the created devices:
 sudo modprobe ashmem_linux
 sudo modprobe binder_linux
 lsmod | grep -e ashmem_linux -e binder_linux
 ls -alh /dev/binder /dev/ashmem
+'
--- REMOVE.sh
+++ REMOVE.sh
@@ -1,6 +1,9 @@
+echo 'Removing anbox-ashmem..'
 sudo dkms remove anbox-ashmem/1
+echo 'Removing anbox-binder..'
 sudo dkms remove anbox-binder/1
 
+echo 'Cleaning dirs..'
 sudo rm /etc/modules-load.d/anbox.conf
 sudo rm /etc/modules-load.d/anbox.conf           
 sudo rm /lib/udev/rules.d/99-anbox.rules
